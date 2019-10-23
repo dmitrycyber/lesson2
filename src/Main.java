39d69ed0b11c70f7fc1a1681f8d4e95f82c38a32
Dmitry <dmitry.ivoninsky@gmail.com>
2019-10-23 18:56:12 +0300

    solved tasks 4, 6-10

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import sun.nio.cs.ext.MacArabic;
+
 import java.util.Arrays;
 
 public class Main {
     public static void main (String[] args) {
         /*System.out.println(isPalindrome("йцууцй"));
         System.out.println(Arrays.toString(makeArrayWithRandomElements(10)));
-        System.out.println(Arrays.toString(makeArrayWithRandomElements2(10)));*//*
-        System.out.println(countOfElement(3, makeArrayWithRandomElements(10)));*//*
-        System.out.println(Arrays.toString(indexesOfElement(4, makeArrayWithRandomElements(10))) );*/
-        System.out.println(averageOfArrayElements(makeArrayWithRandomElements(10)));
+        System.out.println(Arrays.toString(makeArrayWithRandomElements2(10)));
+        System.out.println(countOfElement(3, makeArrayWithRandomElements(10)));
+        System.out.println(Arrays.toString(indexesOfElement(4, makeArrayWithRandomElements(10))) );
+        System.out.println(averageOfArrayElements(makeArrayWithRandomElements(10)));*/
+        /*System.out.println(minElementInArray(makeArrayWithRandomElements(10)));
+        System.out.println(maxElementInArray(makeArrayWithRandomElements(10)));*/
+        //swapMinAndMaxElement(makeArrayWithRandomElements(10));
+        /*System.out.println(countOfNegativeElementsInArray(makeArrayWithRandomElements(10)));*/
+        /*System.out.println(Arrays.toString(arrayWithoutElement(2, makeArrayWithRandomElements(10))));*/
+        //reverseArray(makeArrayWithRandomElements(11));
+        /*int[] array = {1, 2, 3, 4, 5};
+        System.out.println(isArraySortedAsc(array));*/
+        randomSortingArray(makeArrayWithRandomElements(10));
+
 
 
     }
@@ -81,6 +94,119 @@ public class Main {
         System.out.println(sumOfElements);
         System.out.println(givenArray.length);
         return (float) sumOfElements / givenArray.length;
+    }
+
+    public static int minElementInArray(int[] givenArray) {
+        int minElement = givenArray[0];
+        for (int i = 1; i < givenArray.length; i++) {
+            if (givenArray[i] < minElement) {
+                minElement = givenArray[i];
+            }
+        }
+        return minElement;
+    }
+
+    public static int maxElementInArray (int[] givenArray) {
+        int maxElement = givenArray[0];
+        for (int i = 1; i < givenArray.length; i++) {
+            if (givenArray[i] > maxElement) {
+                maxElement = givenArray[i];
+            }
+        }
+        return maxElement;
+    }
+
+    public static int[] swapMinAndMaxElement (int[] givenArray) {
+        System.out.println("Before");
+        System.out.println(Arrays.toString(givenArray));
+        int maxElement = maxElementInArray(givenArray);
+        int minElement = minElementInArray(givenArray);
+        System.out.println("Max " + maxElement);
+        System.out.println("Min " + minElement);
+        for (int i = 0; i < givenArray.length; i++) {
+            if (givenArray[i] == minElement) {
+                givenArray[i] = maxElement;
+            }
+            else if (givenArray[i] == maxElement) {
+                maxElement = minElement;
+                givenArray[i] = maxElement;
+            }
+        }
+        System.out.println("After");
+        System.out.println(Arrays.toString(givenArray));
+        return givenArray;
+    }
+
+    public static int countOfNegativeElementsInArray (int[] givenArray) {
+        int count = 0;
+        for (int i = 0; i < givenArray.length; i++) {
+            if (givenArray[i] < 0) {
+                count ++;
+            }
+        }
+        return count;
+    }
+
+    public static int[] arrayWithoutElement (int indexOfElement, int[] givenArray) {
+        System.out.println(Arrays.toString(givenArray));
+        int[] array = new int[givenArray.length - 1];
+        int indexOfElementInGivenArray = 0;
+        int indexOfElementInArray = 0;
+        while (indexOfElementInGivenArray < givenArray.length && indexOfElementInArray < array.length) {
+            if (indexOfElementInGivenArray == indexOfElement) {
+                indexOfElementInGivenArray++;
+            }
+            else {
+                array[indexOfElementInArray] = givenArray[indexOfElementInGivenArray];
+                indexOfElementInGivenArray++;
+                indexOfElementInArray++;
+            }
+        }
+        return array;
+    }
+
+    public static int[] reverseArray (int[] givenArray) {
+        System.out.println(Arrays.toString(givenArray));
+        int counter = givenArray.length - 1;
+        int tmp;
+        for (int i = 0; i < givenArray.length/2; i++) {
+            tmp = givenArray[counter];
+            givenArray[counter] = givenArray[i];
+            givenArray[i] = tmp;
+            counter--;
+        }
+        return givenArray;
+    }
+
+    public static boolean isArraySortedAsc (int[] givenArray) {
+        System.out.println(Arrays.toString(givenArray));
+        boolean isSortedAsc = false;
+        for (int i = 0; i < givenArray.length - 1; i++) {
+            if (givenArray[i] < givenArray[i+1]) {
+                isSortedAsc = true;
+            }
+            else {
+                isSortedAsc = false;
+            }
+        }
+        return isSortedAsc;
+    }
+
+    public static void randomSortingArray (int[] givenArray) {
+        System.out.println(Arrays.toString(givenArray));
+        int[] array = new int[givenArray.length];
+        boolean[] isIndexFull = new boolean[givenArray.length];
+        isIndexFull[0] = true;
+        int randomIndex = (int) (Math.random() * givenArray.length - 1);
+        for (int i = 0; i < givenArray.length; i++) {
+            while (isIndexFull[randomIndex] == true) {
+                randomIndex = (int) (Math.random() * givenArray.length - 1);
+                if
+            }
+            array[i] = givenArray[randomIndex];
+            isIndexFull[randomIndex] = true;
+        }
+        System.out.println(Arrays.toString(array));
     }
 
 }
